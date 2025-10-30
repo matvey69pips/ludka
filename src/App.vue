a37e680e943479e5d0ba26e64cd1ae09b4b0bb8d
matvey69pips <mlemesev42@gmail.com>
2025-10-30 18:55:39 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="title">Lydka</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+
+
+<style scoped>
+.title{
+  text-align: center;
+  font-size: 60px;
+  font-family: 'Times New Roman', Times, serif;
+  background-image: linear-gradient(to top, rgb(169, 43, 43), rgb(247, 14, 14));;
+}
+
+</style>
